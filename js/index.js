7d8386eebb4fa740610f9f8eb11d5529b64117a5
C1zar1 <lutsenko319@gmail.com>
2026-03-03 16:04:27 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let items = [
     {name: "keyboard A4Tech Bloody S98 Naraka BLMS USB Black/White/Red", price: 100, img: "image/KeyBoard.png"},
     {name: "ASUS TUF Gaming A16 (2025) FA608UM-RV015 (90NR0KV1-M00880)", price: 1500, img: "image/Gaming_laptop.png"},
     {name: "Lenovo LOQ 15ARP9 (83JC00K6RA) Luna Gray Gaming Laptop", price: 15000, img: "image/Gaming_laptop2.jpg"},
-    {name: "Lenovo LOQ 15ARP9 (83JC00K6RA) Luna Gray Gaming Laptop", price: 1500000000000000000000, img: "image/Sonic.jpg"}
+    {name: "Sonic😰😰😰😰😰😰", price: 1500000000000000000000, img: "image/Sonic.jpg"}
 ];
 let cart = [];
 function showProducts() {
